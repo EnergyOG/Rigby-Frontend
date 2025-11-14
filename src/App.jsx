03909986f9749b134ca2f2e5ae32396cfe45d3be
Dennis Peprah <macbookpro@Dennis-MacBook-Pro-2.local>
2025-11-14 12:38:55 +0000

    updated registration Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./users/components/auth/LoginPage";
 import RegisterPage from "./users/components/auth/RegisterPage";
 import TopBar from "./users/components/TopBar";
+import { Outlet } from "react-router-dom";
 
 function MainLayout() {
-  return <TopBar/>
+  return (
+    <>
+      <TopBar />
+      <Outlet />
+    </>
+  );
 }
 
 function App() {
   return (
     <div className="h-[100vh] bg-gray-400">
       <Routes>
+        {/* Public Routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
+        {/* Protected/Main App Layout */}
         <Route path="/*" element={<MainLayout />} />
       </Routes>
-
     </div>
   );
 }
--- src/users/components/TopBar.jsx
+++ src/users/components/TopBar.jsx
@@ -11,25 +11,25 @@ import RightSidebar from "./RightSidebar";
 
 function TopBar() {
   return (
-    <div className="box-border h-[100vh] overflow-y-hidden">
+    <div className="box-border h-[100vh]">
       <div className="bg-white fixed w-full flex justify-between items-center py-1 px-20">
         <div className="flex items-center">
           <img
             src={logo}
             alt="logo"
-            className="w-13 h-auto border rounded-md py-1 px-1 bg-white"
+            className="w-[52px] h-auto border rounded-md py-1 px-1 bg-white"
           />
           <img
             src={tagline}
-            alt="logo"
-            className="w-25 h-9 border rounded-md py-1 px-1 bg-white"
+            alt="tagline"
+            className="w-[100px] h-[36px] border rounded-md py-1 px-1 bg-white"
           />
         </div>
-        <div className="flex gap-6 border-2 h-10 border-none items-center">
+        <div className="flex gap-6 h-10 items-center">
           <NavLink
             to="/live"
             className={({ isActive }) =>
-              isActive ? "text-blue-500 border-b-3" : "hover:text-blue-500"
+              isActive ? "text-blue-500 border-b-[3px]" : "hover:text-blue-500"
             }
           >
             Live
@@ -37,7 +37,7 @@ function TopBar() {
           <NavLink
             to="/trending"
             className={({ isActive }) =>
-              isActive ? "text-blue-500 border-b-3" : "hover:text-blue-500"
+              isActive ? "text-blue-500 border-b-[3px]" : "hover:text-blue-500"
             }
           >
             Trending
@@ -45,7 +45,7 @@ function TopBar() {
           <NavLink
             to="/feed"
             className={({ isActive }) =>
-              isActive ? "text-blue-500 border-b-3" : "hover:text-blue-500"
+              isActive ? "text-blue-500 border-b-[3px]" : "hover:text-blue-500"
             }
           >
             Feed
@@ -53,7 +53,7 @@ function TopBar() {
           <NavLink
             to="/newest"
             className={({ isActive }) =>
-              isActive ? "text-blue-500 border-b-3" : "hover:text-blue-500"
+              isActive ? "text-blue-500 border-b-[3px]" : "hover:text-blue-500"
             }
           >
             Newest
@@ -61,22 +61,25 @@ function TopBar() {
         </div>
         <div>Profile</div>
       </div>
-      <div className="flex pt-15 h-full">
+
+      <div className="flex pt-[60px] h-full overflow-y-auto">
         <div className="w-[25%]">
-          <LeftSidebar/>
+          <LeftSidebar />
         </div>
+
         <div className="w-[50%] border-gray-300 border">
-        <Routes>
-          <Route path="/" element={<FeedPage />} />
-          <Route path="/live" element={<LivePage />} />
-          <Route path="/trending" element={<TrendingPage />} />
-          <Route path="/feed" element={<FeedPage />} />
-          <Route path="/newest" element={<NewestPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<FeedPage />} />
+            <Route path="/live" element={<LivePage />} />
+            <Route path="/trending" element={<TrendingPage />} />
+            <Route path="/feed" element={<FeedPage />} />
+            <Route path="/newest" element={<NewestPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
         </div>
+
         <div className="w-[25%]">
-          <RightSidebar/>
+          <RightSidebar />
         </div>
       </div>
     </div>
